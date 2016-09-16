5b2a3aa9de1408b8af5a75391b3fa3b11fa861c0
Johan Larsson <johanlarsson@outlook.com>
2016-09-16 13:19:41 +0200

    Work out how to place labels appropriately.

--- R/plotting.R
+++ R/plotting.R
@@ -1,9 +1,10 @@
 #' Plot Euler diagrams
 #'
-#' Generic plotting of euler diagrams created with eulerr using base R graphics.
+#' Plot euler diagrams using base R graphics.
 #'
-#' @param eulerr euler diagram specifications of type \code{'eulerr'}
-#' @param pal Palette from RColorBrewer.
+#' @param eulerr euler diagram specifications of class \code{'eulerr'}
+#' @param col Colors to fill the circles with. The default uses the
+#'   "Accent" palette from RColorBrewer.
 #' @param alpha Alpha for the colors to be plotted
 #' @return Plots euler diagram using Base R graphics.
 #' @examples
@@ -12,18 +13,37 @@
 #'
 #' @export
 
-plot.eulerr <- function(eulerr, pal = "Accent", alpha = .4, ...) {
-  x <- eulerr[["Circles"]][, 1]
-  y <- eulerr[["Circles"]][, 2]
-  r <- eulerr[["Circles"]][, 3]
+plot.eulerr <- function(eulerr, alpha = .4, ...) {
+  x <- fit[["circles"]][, 1]
+  y <- fit[["circles"]][, 2]
+  r <- fit[["circles"]][, 3]
 
-  n <- ifelse(length(x) < 3, 3, length(x))
+  # Find good positions for the text centers
+  text_x <- double(length(x))
+  text_y <- double(length(y))
 
-  pal <- grDevices::adjustcolor(RColorBrewer::brewer.pal(length(x), pal),
-                                alpha.f = alpha)
+  rpoints <- randtoolbox::sobol(100, 2)
+  u <- scale_runif(rpoints[, 1], 0, 2 * pi)
+  v <- sqrt(rpoints[, 2])
+
+  for (i in seq_along(r)) {
+    rs <- scale_runif(v, 0, r[i])
+    xs <- x[i] + rs * cos(u)
+    ys <- y[i] + rs * sin(u)
+
+    locs <- colSums(apply(cbind(xs, ys), 1, find_sets_containing_points,
+                          x, y, r))
+
+    outskirts <- which(locs == min(locs))
+
+    text_x[i] <- mean(xs[outskirts])
+    text_y[i] <- mean(ys[outskirts])
+  }
+
+  pal <- grDevices::adjustcolor(color_palette, alpha = alpha)
   u <- seq(0, 2 * pi, length = 500)
 
-  plot(
+  graphics::plot(
     NULL,
     axes = F,
     ann = F,
@@ -33,19 +53,31 @@ plot.eulerr <- function(eulerr, pal = "Accent", alpha = .4, ...) {
   )
 
   for (i in seq_along(x)) {
-    polygon(
+    graphics::polygon(
       r[i] * cos(u) + x[i],
       r[i] * sin(u) + y[i],
-      col = pal[i],
-      ...
+      col = pal[i]
     )
   }
   for (i in seq_along(x)) {
-    text(
-      x[i],
-      y[i],
-      labels = names(r)[i],
-      ...
+    graphics::text(
+      text_x[i],
+      text_y[i],
+      labels = names(r)[i]
     )
   }
+}
+
+
+# Color palette from qualpalr ---------------------------------------------
+
+color_palette <- c("#6ACB69", "#C96CC6", "#76AACE", "#CF9D74", "#E4CCDD",
+                   "#C0EADF", "#6A71CC", "#E0E1A8", "#C86D7A", "#AFA1DD",
+                   "#C19DA0", "#77BDA5", "#C6D4E8", "#E6CCB9", "#E1A7DC",
+                   "#B0C2A3", "#C8B778", "#8BCDD7", "#9F79CC", "#7391CE")
+
+# Helper functions --------------------------------------------------------
+
+scale_runif <- function(x, new_min, new_max) {
+  (new_max - new_min) * (x - 1) + new_max
 }
