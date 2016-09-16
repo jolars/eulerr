3d9d6590cda683945e2bf9c347b23a0b69191b89
Johan Larsson <johanlarsson@outlook.com>
2016-09-17 00:29:21 +0200

    Document changes.

--- R/plotting.R
+++ R/plotting.R
@@ -2,37 +2,35 @@
 #'
 #' Plot euler diagrams using base R graphics.
 #'
-#' @param eulerr euler diagram specifications of class \code{'eulerr'}
-#' @param col Colors to fill the circles with. The default uses the
-#'   "Accent" palette from RColorBrewer.
+#' @param x euler diagram specifications of class \code{'eulerr'}
 #' @param alpha Alpha for the colors to be plotted
+#' @param ... Arguments to pass to text, polygon, or plot.
 #' @return Plots euler diagram using Base R graphics.
 #' @examples
 #' fit <- eulerr(c(A = 10, B = 5, "A&B" = 3))
-#' plot(fit, pal = "Set2", alpha = .3)
+#' plot(fit, alpha = .7)
 #'
 #' @export
 
-plot.eulerr <- function(eulerr, alpha = .4, ...) {
-  x <- eulerr[["circles"]][, 1]
-  y <- eulerr[["circles"]][, 2]
-  r <- eulerr[["circles"]][, 3]
+plot.eulerr <- function(x, alpha = .4, ...) {
+  X <- x[["circles"]][, 1]
+  Y <- x[["circles"]][, 2]
+  r <- x[["circles"]][, 3]
 
   # Find good positions for the text centers
-  text_x <- double(length(x))
-  text_y <- double(length(y))
+  text_x <- double(length(X))
+  text_y <- double(length(Y))
 
   rpoints <- randtoolbox::sobol(100, 2)
-  u <- scale_runif(rpoints[, 1], 0, 2 * pi)
+  u <- 2 * pi * (rpoints[, 1] - 1) + 2 * pi
   v <- sqrt(rpoints[, 2])
-
   for (i in seq_along(r)) {
-    rs <- scale_runif(v, 0, r[i])
-    xs <- x[i] + rs * cos(u)
-    ys <- y[i] + rs * sin(u)
+    rs <- r[i] * (v - 1) + r[i]
+    xs <- X[i] + rs * cos(u)
+    ys <- Y[i] + rs * sin(u)
 
     locs <- colSums(apply(cbind(xs, ys), 1, find_sets_containing_points,
-                          x, y, r))
+                          X, Y, r))
 
     outskirts <- which(locs == min(locs))
 
@@ -47,19 +45,19 @@ plot.eulerr <- function(eulerr, alpha = .4, ...) {
     NULL,
     axes = F,
     ann = F,
-    xlim = range(c(x + r, x - r)),
-    ylim = range(c(y + r, y - r)),
+    xlim = range(c(X + r, X - r)),
+    ylim = range(c(Y + r, Y - r)),
     asp = 1
   )
 
-  for (i in seq_along(x)) {
+  for (i in seq_along(X)) {
     graphics::polygon(
-      r[i] * cos(u) + x[i],
-      r[i] * sin(u) + y[i],
+      r[i] * cos(u) + X[i],
+      r[i] * sin(u) + Y[i],
       col = pal[i]
     )
   }
-  for (i in seq_along(x)) {
+  for (i in seq_along(X)) {
     graphics::text(
       text_x[i],
       text_y[i],
@@ -76,8 +74,3 @@ color_palette <- c("#6ACB69", "#C96CC6", "#76AACE", "#CF9D74", "#E4CCDD",
                    "#C19DA0", "#77BDA5", "#C6D4E8", "#E6CCB9", "#E1A7DC",
                    "#B0C2A3", "#C8B778", "#8BCDD7", "#9F79CC", "#7391CE")
 
-# Helper functions --------------------------------------------------------
-
-scale_runif <- function(x, new_min, new_max) {
-  (new_max - new_min) * (x - 1) + new_max
-}
