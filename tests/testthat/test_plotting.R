5289b6da242f44e9eefdfa5ab6da516c08d607d3
Johan Larsson <johanlarsson@outlook.com>
2016-09-18 15:04:17 +0200

    Test plot and residual methods.

--- tests/testthat/test_plotting.R
+++ tests/testthat/test_plotting.R
@@ -1,5 +1,6 @@
 context("Test plot functions")
 
 test_that("erroneous input to plotting functions return errors", {
-  expect_error(plot.eulerr(2))
+  expect_error(eulerr:::plot.eulerr(2))
+  expect_error(eulerr:::residuals.eulerr(2))
 })
