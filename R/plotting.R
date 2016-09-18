2c917a7560b3427ae4c39b9f92482ec191e28b6c
Johan Larsson <johanlarsson@outlook.com>
2016-09-18 12:04:46 +0200

    Redesign plot function to allow customization.

--- R/plotting.R
+++ R/plotting.R
@@ -1,21 +1,88 @@
 #' Plot Euler diagrams
 #'
-#' Plot euler diagrams using base R graphics.
+#' Plot Euler diagrams using base R graphics.
 #'
-#' @param x euler diagram specifications of class \code{'eulerr'}
-#' @param alpha Alpha for the colors to be plotted
-#' @param ... Arguments to pass to text, polygon, or plot.
-#' @return Plots euler diagram using Base R graphics.
+#' If no color is specified to \code{polygon_args}, fills will be colored
+#' using a predefined palette taken from \code{\link[qualpalr]{qualpal}}.
+#'
+#' @param x Specifications for a Euler diagram in the form of a object of class
+#'   \code{'eulerr'}
+#' @param fill_opacity Opacity for the fill colors between 0 and 1. Values
+#'   beyond this range will be clamped to [0, 1] without warning.
+#' @param polygon_args Arguments for \code{\link[graphics]{polygon}},
+#'   which is used to draw the circles.
+#' @param text_args Arguments for \code{\link[graphics]{text}},
+#'   which is used to draw the text.
+#' @param ... Arguments for \code{\link[graphics]{plot}}, which is used to draw
+#'   the plot area.
+#' @return Plots euler diagram using base R graphics.
+#' @seealso \code{\link[graphics]{plot}}, \code{\link[graphics]{polygon}},
+#'   \code{\link[graphics]{text}}
 #' @examples
 #' fit <- eulerr(c(A = 10, B = 5, "A&B" = 3))
-#' plot(fit, alpha = .7)
+#' plot(fit, fill_opacity = .7)
+#'
+#' # Change to italic roman font, remove borders and switch colors
+#' plot(fit,
+#'      polygon_args = list(col = c("dodgerblue4", "darkgoldenrod1"),
+#'                          border = "transparent"),
+#'      text_args = list(font = 8))
+#'
+#' # Plot without fills and distinguish sets with border types instead
+#' plot(fit,
+#'      polygon_args = list(lty = c("solid", "dotted"), col = "transparent"),
+#'      text_args = list(cex = 2, font = 2))
+#'
+#' # Slim down margins and surround plot with a box
+#' opar <- par(no.readonly = TRUE)
+#' par(mar = c(0.1, 0.1, 0.1, 0.1))
+#' plot(fit)
+#' box()
+#' par(opar)
 #'
 #' @export
+#' @import assertthat
 
-plot.eulerr <- function(x, alpha = .4, ...) {
+plot.eulerr <- function(x, fill_opacity = 0.4,  polygon_args = list(),
+                        text_args = list(), ...) {
+  assert_that(inherits(x, "eulerr"))
+
   X <- x[["circles"]][, 1]
   Y <- x[["circles"]][, 2]
   r <- x[["circles"]][, 3]
+
+  graphics::plot(
+    NULL,
+    axes = F,
+    ann = F,
+    xlim = range(c(X + r, X - r)),
+    ylim = range(c(Y + r, Y - r)),
+    asp = 1,
+    ...
+  )
+
+  g <- seq(0, 2 * pi, length = 500)
+  x_coords <- double(0)
+  y_coords <- double(0)
+  for (i in seq_along(X)) {
+    x_coords <- c(x_coords, r[i] * cos(g) + X[i], NA)
+    y_coords <- c(y_coords, r[i] * sin(g) + Y[i], NA)
+  }
+
+  # Use color palette from qualpalr if none was specified.
+  if (is.null(polygon_args$col)) {
+    polygon_args$col <- c("#6ACB69", "#C96CC6", "#76AACE", "#CF9D74", "#E4CCDD",
+                          "#C0EADF", "#6A71CC", "#E0E1A8", "#C86D7A", "#AFA1DD",
+                          "#C19DA0", "#77BDA5", "#C6D4E8", "#E6CCB9", "#E1A7DC",
+                          "#B0C2A3", "#C8B778", "#8BCDD7", "#9F79CC", "#7391CE")
+  }
+
+  polygon_args$col <- grDevices::adjustcolor(polygon_args$col,
+                                             alpha = fill_opacity)
+  polygon_args$x <- x_coords
+  polygon_args$y <- y_coords
+
+  do.call(graphics::polygon, polygon_args)
 
   # Find good positions for the text centers
   text_x <- double(length(X))
@@ -33,44 +100,14 @@ plot.eulerr <- function(x, alpha = .4, ...) {
                           X, Y, r))
 
     outskirts <- which(locs == min(locs))
-
     text_x[i] <- mean(xs[outskirts])
     text_y[i] <- mean(ys[outskirts])
   }
 
-  pal <- grDevices::adjustcolor(color_palette, alpha = alpha)
-  u <- seq(0, 2 * pi, length = 500)
+  text_args$x <- text_x
+  text_args$y <- text_y
+  text_args$labels <- names(r)
 
-  graphics::plot(
-    NULL,
-    axes = F,
-    ann = F,
-    xlim = range(c(X + r, X - r)),
-    ylim = range(c(Y + r, Y - r)),
-    asp = 1
-  )
-
-  for (i in seq_along(X)) {
-    graphics::polygon(
-      r[i] * cos(u) + X[i],
-      r[i] * sin(u) + Y[i],
-      col = pal[i]
-    )
-  }
-  for (i in seq_along(X)) {
-    graphics::text(
-      text_x[i],
-      text_y[i],
-      labels = names(r)[i]
-    )
-  }
+  do.call(graphics::text, text_args)
 }
-
-
-# Color palette from qualpalr ---------------------------------------------
-
-color_palette <- c("#6ACB69", "#C96CC6", "#76AACE", "#CF9D74", "#E4CCDD",
-                   "#C0EADF", "#6A71CC", "#E0E1A8", "#C86D7A", "#AFA1DD",
-                   "#C19DA0", "#77BDA5", "#C6D4E8", "#E6CCB9", "#E1A7DC",
-                   "#B0C2A3", "#C8B778", "#8BCDD7", "#9F79CC", "#7391CE")
 
