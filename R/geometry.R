72d2b668fb462e14762d395ed4a2888b09a0cbdc
Johan Larsson <johanlarsson@outlook.com>
2016-10-23 14:26:23 +0200

    Start using L to improve speed.

--- R/geometry.R
+++ R/geometry.R
@@ -1,14 +1,14 @@
 # Compute the overlap of two circles
 intersect_two_discs <- function(r1, r2, d) {
-  r1 ^ 2 * acos((d ^ 2 + r1 ^ 2 - r2 ^ 2) / (2 * d * r1)) +
-  r2 ^ 2 * acos((d ^ 2 + r2 ^ 2 - r1 ^ 2) / (2 * d * r2)) -
+  r1 ^ 2L * acos((d ^ 2L + r1 ^ 2L - r2 ^ 2L) / (2L * d * r1)) +
+  r2 ^ 2L * acos((d ^ 2L + r2 ^ 2L - r1 ^ 2L) / (2L * d * r2)) -
   0.5 * sqrt((r1 + r2 - d) * (d + r1 - r2) * (d - r1 + r2) * (d + r1 + r2))
 }
 
 # Find intersection points
 locate_intersections <- function(r1, r2, x_d, y_d, x_c, y_c, d) {
-  l  <- (r2 ^ 2 - r1 ^ 2 + d ^ 2) / (2 * d)
-  h  <- sqrt(r2 ^ 2 - l ^ 2)
+  l  <- (r2 ^ 2L - r1 ^ 2L + d ^ 2L) / (2L * d)
+  h  <- sqrt(r2 ^ 2L - l ^ 2L)
   ld <- l / d
   hd <- h / d
   x1 <- x_d * ld + y_d * hd + x_c
@@ -26,7 +26,7 @@ find_polygon_area <- function(x, y, n) {
     area[i] <- (x[j] + x[i]) * (y[j] - y[i])
     j <- i
   }
-  sum(area) / 2
+  sum(area) / 2L
 }
 
 # Compute the overlap of three or more circles
@@ -42,14 +42,14 @@ find_threeplus_areas <- function(x_int, y_int, radiuses, circles) {
   for (i in 1:n) {
     circle_now <- circles[, i][circles[, i] %in% circles[, j]]
 
-    d <- sqrt((x_int[j] - x_int[i]) ^ 2 + (y_int[j] - y_int[i]) ^ 2)
+    d <- sqrt((x_int[j] - x_int[i]) ^ 2L + (y_int[j] - y_int[i]) ^ 2L)
     r <- radiuses[circle_now]
 
     # Find angle from center to segment
-    u <- 2 * asin(d / (2 * r))
+    u <- 2L * asin(d / (2L * r))
 
     # Find area of circle segment, in case we have to competing circles
-    A <- ((r ^ 2) / 2) * (u - sin(u))
+    A <- ((r ^ 2L) / 2L) * (u - sin(u))
 
     # Pick the smallest area in case there are two competing areas
     arc_areas[i] <- A[which.min(A)]
@@ -62,5 +62,5 @@ find_threeplus_areas <- function(x_int, y_int, radiuses, circles) {
 find_sets_containing_points <- function (points, x, y, r) {
   x_int <- points[1]
   y_int <- points[2]
-  (x_int - x) ^ 2 + (y_int - y) ^ 2 < r ^ 2
+  (x_int - x) ^ 2L + (y_int - y) ^ 2L < r ^ 2L
 }
