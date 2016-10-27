6da45b2ce9a4b5db4fa3c193063cc0981e95f280
Johan Larsson <johanlarsson@outlook.com>
2016-10-27 12:45:10 +0200

    Switch to pmin.int, use use.names in unlist.

--- R/optimization.R
+++ R/optimization.R
@@ -1,8 +1,8 @@
 # Initial optimizer
 initial_layout_optimizer <- function(par, distances, disjoint, contained, two) {
-  pars <- matrix(par, ncol = 2)
-  x    <- pars[, 1]
-  y    <- pars[, 2]
+  m    <- matrix(par, ncol = 2)
+  x    <- m[, 1]
+  y    <- m[, 2]
   x_d  <- x[two[1, ]] - x[two[2, ]]
   y_d  <- y[two[1, ]] - y[two[2, ]]
   d    <- x_d ^ 2 + y_d ^ 2
@@ -12,23 +12,23 @@ initial_layout_optimizer <- function(par, distances, disjoint, contained, two) {
 
 # Gradient for initial optimizer
 initial_layout_gradient <- function(par, distances, disjoint, contained, two) {
-  pars <- matrix(par, ncol = 2)
-  x    <- pars[, 1]
-  y    <- pars[, 2]
-  x_d  <- x[two[1, ]] - x[two[2, ]]
-  y_d  <- y[two[1, ]] - y[two[2, ]]
-  d    <- x_d ^ 2 + y_d ^ 2
-  i    <- !((d >= distances ^ 2 & disjoint) | (d <= distances ^ 2 & contained))
+  m   <- matrix(par, ncol = 2)
+  x   <- m[, 1]
+  y   <- m[, 2]
+  x_d <- x[two[1, ]] - x[two[2, ]]
+  y_d <- y[two[1, ]] - y[two[2, ]]
+  d   <- x_d ^ 2 + y_d ^ 2
+  i   <- !((d >= distances ^ 2 & disjoint) | (d <= distances ^ 2 & contained))
 
   grad_x    <- grad_y <- double(length(i))
-  grad_x[i] <- 4L * (d[i] - distances[i] ^ 2L) * (x_d[i])
-  grad_y[i] <- 4L * (d[i] - distances[i] ^ 2L) * (y_d[i])
+  grad_x[i] <- 4L * (d[i] - distances[i] ^ 2L) * x_d[i]
+  grad_y[i] <- 4L * (d[i] - distances[i] ^ 2L) * y_d[i]
   grad_x    <- rbind(grad_x, -grad_x)
   grad_y    <- rbind(grad_y, -grad_y)
 
-  c(vapply(seq_along(x), function(x) {sum(grad_x[two == x])},
+  c(vapply(seq_along(x), function(x) sum(grad_x[two == x]),
            FUN.VALUE = double(1L)),
-    vapply(seq_along(y), function(y) {sum(grad_y[two == y])},
+    vapply(seq_along(y), function(y) sum(grad_y[two == y]),
            FUN.VALUE = double(1L)))
 }
 
@@ -53,7 +53,7 @@ final_layout_optimizer <- function(par, areas, id) {
   fit <- return_intersections(par, areas, id)
   y <- unlist(areas)
   TSS <- sum((y - y / length(y)) ^ 2L)
-  SSE <- sum((y - unlist(fit)) ^ 2L)
+  SSE <- sum((y - unlist(fit, use.names = FALSE)) ^ 2L)
 
   if (TSS > 0) {
     SSE / TSS
@@ -64,29 +64,29 @@ final_layout_optimizer <- function(par, areas, id) {
 
 # Return areas from x, y, etc.
 return_intersections <- function(par, areas, id) {
-  pars <- matrix(par, ncol = 3L)
-  x    <- pars[, 1L]
-  y    <- pars[, 2L]
-  r    <- pars[, 3L]
-  two  <- id[[2L]]
+  m   <- matrix(par, ncol = 3)
+  x   <- m[, 1]
+  y   <- m[, 2]
+  r   <- m[, 3]
+  two <- id[[2]]
 
   for (i in seq_along(areas)) areas[[i]] <- 0L
 
   # Fill in the one set areas
-  areas[[1L]] <- r ^ 2L * pi
+  areas[[1]] <- r ^ 2L * pi
 
-  x_c <- matrix(x[two], nrow = 2L)
-  y_c <- matrix(y[two], nrow = 2L)
-  x_d <- x[two[1L, ]] - x[two[2L, ]]
-  y_d <- y[two[1L, ]] - y[two[2L, ]]
+  x_c <- matrix(x[two], nrow = 2)
+  y_c <- matrix(y[two], nrow = 2)
+  x_d <- x[two[1, ]] - x[two[2, ]]
+  y_d <- y[two[1L, ]] - y[two[2, ]]
   d   <- sqrt(x_d ^ 2L + y_d ^ 2L)
-  r1  <- r[two[1L, ]]
-  r2  <- r[two[2L, ]]
+  r1  <- r[two[1, ]]
+  r2  <- r[two[2, ]]
 
   contained    <- d <= abs(r1 - r2)
   disjoint     <- d >= r1 + r2
   intersecting <- !(disjoint | contained)
-  areas[[2]][contained]    <- pmin(r1[contained], r2[contained]) ^ 2L * pi
+  areas[[2]][contained]    <- pmin.int(r1[contained], r2[contained]) ^ 2L * pi
   areas[[2]][disjoint]     <- 0L
   areas[[2]][intersecting] <- intersect_two_discs(r1 = r1[intersecting],
                                                   r2 = r2[intersecting],
@@ -98,13 +98,13 @@ return_intersections <- function(par, areas, id) {
     r2  = r2[intersecting],
     x_d = x_d[intersecting],
     y_d = y_d[intersecting],
-    x_c = x_c[2L, intersecting],
-    y_c = y_c[2L, intersecting],
+    x_c = x_c[2, intersecting],
+    y_c = y_c[2, intersecting],
     d   = d[intersecting]
   )
   in_circles <- matrix(FALSE, ncol = nrow(int_points), nrow = length(x))
   in_circles[, intersecting] <- apply(int_points[intersecting, ],
-                                      1L, find_sets_containing_points, x, y, r)
+                                      1, find_sets_containing_points, x, y, r)
   twoway_int <- cbind(two[, intersecting], two[, intersecting])
   for (i in seq_along(twoway_int[1, ])) {
     in_circles[, intersecting][twoway_int[, i], i] <- TRUE
@@ -112,11 +112,11 @@ return_intersections <- function(par, areas, id) {
   all_circles <- cbind(two, two)
 
   # Iterate over all higher order intersections
-  for (i in seq_along(id[-c(1L, 2L)])) {
+  for (i in seq_along(id[-c(1, 2)])) {
     i <- i + 2L
     for (j in 1L:ncol(id[[i]])) {
       a <- id[[i]][, j]
-      b <- two[1L, ] %in% a & two[2L, ] %in% a
+      b <- two[1, ] %in% a & two[2, ] %in% a
 
       # Which of the intersection points are within all sets?
       in_all <- .colSums(in_circles[a, , drop = FALSE],
@@ -137,13 +137,13 @@ return_intersections <- function(par, areas, id) {
 
       } else if (ncol(circles) == 2L) {
         # Return 2 circle intersecting
-        areas[[i]][j] <- intersect_two_discs(r[circles[1L, 1L]],
-                                             r[circles[2L, 1L]],
-                                             d[in_all][1L])
+        areas[[i]][j] <- intersect_two_discs(r[circles[1, 1]],
+                                             r[circles[2, 1]],
+                                             d[in_all][1])
       } else if (ncol(circles) > 2L) {
         # Return three plus circle intersection
-        areas[[i]][j] <- find_threeplus_areas(x_int = int_points[in_all, 1L],
-                                              y_int = int_points[in_all, 2L],
+        areas[[i]][j] <- find_threeplus_areas(x_int = int_points[in_all, 1],
+                                              y_int = int_points[in_all, 2],
                                               radiuses = r,
                                               circles = circles)
       }
