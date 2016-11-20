860f3a26ff3c6a0ce2548319881235d8354aed9f
Johan Larsson <johanlarsson@outlook.com>
2016-11-20 20:24:09 +0100

    Scrap old R based area computations.

--- R/geometry.R
+++ R/geometry.R
@@ -18,47 +18,7 @@ locate_intersections <- function(r1, r2, x_d, y_d, x_c, y_c, d) {
   cbind(c(x1, x2), c(y1, y2))
 }
 
-# Compute the area of a polygon
-find_polygon_area <- function(x, y, n) {
-  s <- seq_along(x)
-  k <- c(n, s[-n])
-  sum((x[k] + x[s]) * (y[k] - y[s])) / 2L
-}
-
-# Compute the overlap of three or more circles
-find_threeplus_areas <- function(x_int, y_int, radiuses, circles) {
-  # Sort points clockwise from center
-  j <- n <- length(x_int)
-  ux <- sum(x_int) / n
-  uy <- sum(y_int) / n
-  ind <- order(atan2(x_int - ux, y_int - uy), method = "radix")
-  x_int <- x_int[ind]
-  y_int <- y_int[ind]
-  circles <- circles[, ind]
-
-  arc_areas <- double(n)
-  for (i in seq_along(arc_areas)) {
-    circle_now <- circles[, i][circles[, i] %in% circles[, j]]
-
-    d <- sqrt((x_int[j] - x_int[i]) ^ 2L + (y_int[j] - y_int[i]) ^ 2L)
-    r <- radiuses[circle_now]
-
-    # Find angle from center to segment
-    u <- 2L * asin(d / (2L * r))
-
-    # Find area of circle segment, in case we have to competing circles
-    A <- ((r ^ 2L) / 2L) * (u - sin(u))
-
-    # Pick the smallest area in case there are two competing areas
-    arc_areas[i] <- min(A)
-
-    j <- i
-  }
-  sum(arc_areas, find_polygon_area(x_int, y_int, n))
-}
-
-# find
-
+# Find which sets contain some points
 find_sets_containing_points <- function(int_points, x, y, r) {
   t(outer(int_points[, 1], x, function(a, x) (a - x) ^ 2) +
       outer(int_points[, 2], y, function(b, y) (b - y) ^ 2)) <= r ^ 2L
