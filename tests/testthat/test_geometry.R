95af9b962e01f22f731aefa04c70122c6e1e4e94
Johan Larsson <johanlarsson@outlook.com>
2016-12-25 23:39:30 +0100

    Upload tests for the geometry functions.

---
+++ tests/testthat/test_geometry.R
@@ -0,0 +1,10 @@
+context("Test geometry functions")
+
+test_that("that find_sets_containing_points function works", {
+  x_int <- cbind(1:10, 1:10)
+  res <- find_sets_containing_points(x_int, 2, 2, 4)
+  expect_equal(sum(res), 4)
+
+  expect_equal(dist_point_circle(-4, -11, 4, -5, 7), 3)
+
+})
