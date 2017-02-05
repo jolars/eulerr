a695ef0fe28281f0821447ac1e8488b7cc24c671
Johan Larsson <johanlarsson@outlook.com>
2017-02-05 09:29:24 +0100

    Remove defunct tests.

--- tests/testthat/test_plotting.R
+++ tests/testthat/test_plotting.R
@@ -2,27 +2,8 @@ context("Test plot functions")
 
 test_that("erroneous input to plot.euler return errors", {
   f <- euler(c(A = 5, B = 2))
-  expect_error(plot(f, mar = c(2, 2, 2)))
-  expect_error(plot(f, mar = "Hello"))
   expect_error(plot(f, fill_opacity = "f"))
   expect_error(plot(f, fill_opacity = c(2, 3)))
-  expect_error(plot(f, polygon_args = 1))
-  expect_error(plot(f, text_args = 1))
-})
-
-test_that("erroneous input to plot.euler_grid return errors", {
-  dat <- data.frame(
-    A = sample(c(TRUE, FALSE), size = 100, replace = TRUE),
-    B = sample(c(TRUE, TRUE, FALSE), size = 100, replace = TRUE),
-    x = sample(c("Men", "Women"), size = 100, replace = TRUE),
-    y = sample(c("Sweden", "Denmark"), size = 100, replace = TRUE),
-    z = sample(c("asdf", "qwer", size = 100, replace = TRUE))
-  )
-  f2 <- euler(dat[, 1:2], by = dat[, 3:4])
-  expect_error(plot(f2, mfrow = c(1, 2, 3)))
-  expect_error(plot(f2, mfrow = "asdf"))
-  expect_error(plot(f2, main = 1))
-  expect_error(plot(f2, main = c("aa", "a")))
 })
 
 test_that("normal plotting works without errors", {
@@ -30,22 +11,22 @@ test_that("normal plotting works without errors", {
   ff <- tempfile()
   png(filename = ff)
 
-  expect_error(plot(f1, fill_opacity = 0.3, text_args = list(font = 4),
-                    mar = c(1, 2, 1, 2)),
-               NA)
+  expect_error(plot(f1, fill = c("black", "blue"), labels = c("Yes", "No"),
+                    fill_opacity = 0.3, fontface = 4), NA)
 
   dat <- data.frame(
     Liberal = sample(c(TRUE, FALSE), size = 100, replace = TRUE),
     Conservative = sample(c(TRUE, TRUE, FALSE), size = 100, replace = TRUE),
-    gender = sample(c("Men", "Women"), size = 100, replace = TRUE),
-    nation = sample(c("Sweden", "Denmark"), size = 100, replace = TRUE)
+    Gender = sample(c("Men", "Women"), size = 100, replace = TRUE),
+    Nation = sample(c("Sweden", "Denmark"), size = 100, replace = TRUE)
   )
 
   f2 <- euler(dat[, 1:2], by = dat[, 3:4])
+  f3 <- euler(dat[, 1:2], by = dat[, 3])
 
-  expect_error(plot(f2, polygon_args = list(col = "transparent"),
-                    mfrow = c(1, 4), main = c("A", "B", "C", "D")),
+  expect_error(plot(f2, fill = "transparent", lty = c(1, 2), lwd = c(1, 2)),
                NA)
+  expect_error(plot(f3), NA)
 
   dev.off()
   unlink(ff)
