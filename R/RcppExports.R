5ff922b998f18acba8164f574bde2464e7d3c159
Johan Larsson <johanlarsson@outlook.com>
2017-06-06 10:58:05 +0200

    Automatic attribute updates.

--- R/RcppExports.R
+++ R/RcppExports.R
@@ -2,26 +2,26 @@
 # Generator token: 10BE3573-1514-4C36-9D1C-5A225CD40393
 
 bit_index <- function(n) {
-    .Call('eulerr_bit_index', PACKAGE = 'eulerr', n)
+    .Call(eulerr_bit_index, n)
 }
 
 return_intersections <- function(par) {
-    .Call('eulerr_return_intersections', PACKAGE = 'eulerr', par)
+    .Call(eulerr_return_intersections, par)
 }
 
 venneuler_stress <- function(areas, fit) {
-    .Call('eulerr_venneuler_stress', PACKAGE = 'eulerr', areas, fit)
+    .Call(eulerr_venneuler_stress, areas, fit)
 }
 
 loss_final <- function(par, areas) {
-    .Call('eulerr_loss_final', PACKAGE = 'eulerr', par, areas)
+    .Call(eulerr_loss_final, par, areas)
 }
 
 choose_two <- function(x) {
-    .Call('eulerr_choose_two', PACKAGE = 'eulerr', x)
+    .Call(eulerr_choose_two, x)
 }
 
 discdisc <- function(r1, r2, d) {
-    .Call('eulerr_discdisc', PACKAGE = 'eulerr', r1, r2, d)
+    .Call(eulerr_discdisc, r1, r2, d)
 }
 
