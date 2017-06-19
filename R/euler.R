5b5735c56ff8a08f46ca2b347b5431a420ad05c7
Johan Larsson <johanlarsson@outlook.com>
2017-06-19 21:57:39 +0200

    Rename main to euler.

--- R/euler.R
+++ R/euler.R
@@ -4,7 +4,7 @@
 #' optimization to find exact or approximate solutions to a specification of set
 #' relationships.
 #'
-#' If the input is a matrix or data frame and argument \code{by} is specified,
+#' If the input is a matrix or data frame and argument `by` is specified,
 #' the function returns a list of euler diagrams.
 #'
 #' The function minimizes the sums of squared errors between the disjoint areas
@@ -28,8 +28,8 @@
 #' regression of the fitted areas on the original areas that are currently being
 #' explored.
 #'
-#' \code{euler()} also returns \code{diag_error} and \code{region_error} from
-#' \emph{eulerAPE}. \code{region_error} is computed as
+#' `euler()` also returns `diag_error` and `region_error` from
+#' *eulerAPE*. `region_error` is computed as
 #'
 #' \deqn{
 #'   \left| \frac{y_i}{\sum y_i} - \frac{\hat{y}_i}{\sum \hat{y}_i}\right|
@@ -37,15 +37,14 @@
 #'   max|fit / \sum fit  - original / \sum original|
 #' }.
 #'
-#' \code{diag_error} is simply the maximum of region_error.
+#' `diag_error` is simply the maximum of region_error.
 #'
 #' @param combinations Set relationships as a named numeric vector, matrix, or
 #'   data.frame. (See the methods (by class) section for details.)
-#' @param by A factor or character matrix to be used in \code{\link[base]{by}} to
+#' @param by A factor or character matrix to be used in [base::by()] to
 #'   split the data.frame or matrix of set combinations.
 #' @param input The type of input: disjoint class combinations
-#'   (\code{disjoint}) or unions (\code{union}).
-#' @param \dots Currently ignored.
+#'   (`disjoint`) or unions (`union`).
 #'
 #' @return A list object of class 'euler' with the following parameters.
 #'   \item{coefficients}{A matrix of x and y coordinates for the centers of the
@@ -58,7 +57,7 @@
 #'   \item{stress}{The stress of the solution, computed as the sum of squared
 #'     residuals over the total sum of squares.}
 #'
-#' @seealso \code{\link{plot.euler}}, \code{\link{print.euler}}
+#' @seealso [plot.euler()], [print.euler()]
 #'
 #' @examples
 #' # First fit the euler specification
@@ -73,7 +72,7 @@
 #'         "A&B&C" = 0))
 #'
 #' # A euler diagram from a list of sample spaces (the list method)
-#' euler(list(A = c("a", "ab", "ac", "abc"),
+#' f <- euler(list(A = c("a", "ab", "ac", "abc"),
 #'            B = c("b", "ab", "bc", "abc"),
 #'            C = c("c", "ac", "bc", "abc")))
 #'
@@ -99,35 +98,35 @@
 #'
 #' @references Wilkinson L. Exact and Approximate Area-Proportional Circular
 #'   Venn and Euler Diagrams. IEEE Transactions on Visualization and Computer
-#'   Graphics [Internet]. 2012 Feb [cited 2016 Apr 9];18(2):321–31. Available
-#'   from: \url{http://doi.org/10.1109/TVCG.2011.56}
+#'   Graphics (Internet). 2012 Feb (cited 2016 Apr 9);18(2):321–31. Available
+#'   from:
+#'   [http://doi.org/10.1109/TVCG.2011.56](http://doi.org/10.1109/TVCG.2011.56)
 #'
 #'   Micallef L, Rodgers P. eulerAPE: Drawing Area-Proportional 3-Venn Diagrams
-#'   Using Ellipses. PLOS ONE [Internet]. 2014 Jul [cited 2016 Dec
-#'   10];9(7):e101717. Available from:
-#'   \url{http://dx.doi.org/10.1371/journal.pone.0101717}
+#'   Using Ellipses. PLOS ONE (Internet). 2014 Jul (cited 2016 Dec 10);9(7):e101717. Available from:
+#'   [http://dx.doi.org/10.1371/journal.pone.0101717](http://dx.doi.org/10.1371/journal.pone.0101717)
 #'
 #' @export
 euler <- function(combinations, ...) UseMethod("euler")
 
 #' @describeIn euler A named numeric vector, with
-#'   combinations seperated by an ampersand, for instance \code{`A&B` = 10}.
+#'   combinations seperated by an ampersand, for instance `A&B = 10`.
 #'   Missing combinations are treated as being 0.
 #'
 #' @export
-euler.default <- function(combinations, input = c("disjoint", "union"), ...) {
-  assertthat::assert_that(
-    is.numeric(combinations),
-    assertthat::not_empty(combinations),
-    all(combinations >= 0),
-    assertthat::has_attr(combinations, "names"),
-    !any(names(combinations) == ""),
-    !any(duplicated(names(combinations)))
-  )
+euler.default <- function(combinations, input = c("disjoint", "union")) {
+  # Assertions
+  assert_that(is.numeric(combinations),
+              not_empty(combinations),
+              all(combinations >= 0),
+              has_attr(combinations, "names"),
+              !any(names(combinations) == ""),
+              !any(duplicated(names(combinations))))
+
   combo_names <- strsplit(names(combinations), split = "&", fixed = TRUE)
   setnames <- unique(unlist(combo_names, use.names = FALSE))
-  n <- length(setnames)
 
+  n <- length(setnames)
   id <- bit_indexr(n)
 
   areas <- double(nrow(id))
@@ -140,96 +139,98 @@ euler.default <- function(combinations, input = c("disjoint", "union"), ...) {
     }
   }
 
-  # Decompose or collect set volumes depending on input
-  if (match.arg(input) == "disjoint") {
-    areas_disjoint <- areas
-    areas[] <- 0
-
-    for (i in rev(seq_along(areas))) {
-      prev_areas <- rowSums(id[, id[i, ], drop = FALSE]) == sum(id[i, ])
-      areas[i] <- sum(areas_disjoint[prev_areas])
+  if (n > 1) {
+    # Decompose or collect set volumes depending on input
+    if (match.arg(input) == "disjoint") {
+      areas_disjoint <- areas
+      areas[] <- 0
+      for (i in rev(seq_along(areas))) {
+        prev_areas <- rowSums(id[, id[i, ], drop = FALSE]) == sum(id[i, ])
+        areas[i] <- sum(areas_disjoint[prev_areas])
+      }
+    } else if (match.arg(input) == "union") {
+      areas_disjoint <- double(length(areas))
+      for (i in rev(seq_along(areas))) {
+        prev_areas <- rowSums(id[, id[i, ], drop = FALSE]) == sum(id[i, ])
+        areas_disjoint[i] <- areas[i] - sum(areas_disjoint[prev_areas])
+      }
+      if (any(areas_disjoint < 0))
+        stop("Check your set configuration. Your specification resulted in some disjoint areas being set to 0.")
     }
-  } else if (match.arg(input) == "union") {
-    areas_disjoint <- double(length(areas))
 
-    for (i in rev(seq_along(areas))) {
-      prev_areas <- rowSums(id[, id[i, ], drop = FALSE]) == sum(id[i, ])
-      areas_disjoint[i] <- areas[i] - sum(areas_disjoint[prev_areas])
-    }
+    id_sums <- rowSums(id)
+    ones <- id_sums == 1
+    twos <- id_sums == 2
+    two <- choose_two(1:n)
+    r <- sqrt(areas[ones] / pi)
 
-    if (any(areas_disjoint < 0))
-      stop("Check your set configuration. Your specification resulted in some disjoint areas being set to 0.")
+    # Establish identities of disjoint and contained sets
+    disjoint <- areas[twos] == 0
+    tmp <- matrix(areas[ones][two], ncol = 2)
+    contained <- areas[twos] == tmp[, 1] | areas[twos] == tmp[, 2]
+
+    distances <- mapply(separate_two_discs,
+                        r1 = r[two[, 1]],
+                        r2 = r[two[, 2]],
+                        overlap = areas[twos],
+                        USE.NAMES = FALSE)
+
+    # Starting layout
+    initial_layout <- optim(
+      par = runif(n * 2L, 0L, sqrt(sum(r ^ 2L * pi))),
+      fn = initial_layout_optimizer,
+      gr = initial_layout_gradient,
+      distances = distances,
+      disjoint = disjoint,
+      contained = contained,
+      two = two,
+      lower = 0L,
+      upper = sqrt(sum(r ^ 2L * pi)),
+      method = c("L-BFGS-B")
+    )
+
+    # Final layout
+    # TO DO: Allow user customization here?
+    final_layout <- nlm(f = loss_final,
+                        p = c(initial_layout$par, r),
+                        areas = areas_disjoint)
+
+    fit <- as.vector(return_intersections(final_layout$estimate))
+
+    orig <- areas_disjoint
+
+    names(orig) <- names(fit) <-
+      apply(id, 1, function(x) paste0(setnames[x], collapse = "&"))
+
+    region_error <- abs(fit / sum(fit) - orig / sum(orig))
+    diag_error <- max(region_error)
+
+    fpar <- matrix(final_layout$estimate,
+                   ncol = 3,
+                   dimnames = list(setnames, c("x", "y", "r")))
+    stress <- venneuler_stress(orig, fit)
+
+    # Center the solution on the coordinate plane
+    fpar <- center_circles(fpar)
+  } else {
+    # Just one circle
+    fpar <-  matrix(c(0, 0, sqrt(areas / pi)),
+                    ncol = 3,
+                    dimnames = list(setnames, c("x", "y", "r")))
+    region_error <- diag_error <- stress <- 0
+    orig <- fit <- areas
+    names(orig) <- names(fit) <- setnames
   }
 
-  id_sums <- rowSums(id)
-  ones <- id_sums == 1
-  twos <- id_sums == 2
-
-  two <- choose_two(1:n)
-
-  r <- sqrt(areas[ones] / pi)
-
-  # Establish identities of disjoint and contained sets
-  disjoint <- areas[twos] == 0
-  tmp <- matrix(areas[ones][two], ncol = 2)
-  contained <- areas[twos] == tmp[, 1] | areas[twos] == tmp[, 2]
-
-  distances <- mapply(
-    separate_two_discs,
-    r1 = r[two[, 1]],
-    r2 = r[two[, 2]],
-    overlap = areas[twos],
-    USE.NAMES = FALSE
-  )
-
-  # Starting layout
-  initial_layout <- stats::optim(
-    par = stats::runif(n * 2L, 0L, sqrt(sum(r ^ 2L * pi))),
-    fn = initial_layout_optimizer,
-    gr = initial_layout_gradient,
-    distances = distances,
-    disjoint = disjoint,
-    contained = contained,
-    two = two,
-    lower = 0L,
-    upper = sqrt(sum(r ^ 2L * pi)),
-    method = c("L-BFGS-B")
-  )
-
-  final_layout <- stats::nlm(
-    f = loss_final,
-    p = c(initial_layout$par, r),
-    areas = areas_disjoint
-  )
-
-  fit <- as.vector(return_intersections(final_layout$estimate))
-
-  orig <- areas_disjoint
-
-  names(orig) <- names(fit) <-
-    apply(id, 1, function(x) paste0(setnames[x], collapse = "&"))
-
-  region_error <- abs(fit / sum(fit) - orig / sum(orig))
-  diag_error <- max(region_error)
-
-  fpar <- matrix(final_layout$estimate, ncol = 3,
-                 dimnames = list(setnames, c("x", "y", "r")))
-
-  # Center the solution on the coordinate plane
-  fpar <- center_circles(fpar)
-
   # Return eulerr structure
-  structure(
-    list(
-      coefficients = fpar,
-      original.values = orig,
-      fitted.values = fit,
-      residuals = orig - fit,
-      region_error = region_error,
-      diag_error = diag_error,
-      stress = venneuler_stress(orig, fit)
-    ),
-    class = c("euler", "list"))
+  structure(list(coefficients = fpar,
+                 original.values = orig,
+                 fitted.values = fit,
+                 residuals = orig - fit,
+                 region_error = region_error,
+                 diag_error = diag_error,
+                 stress = stress),
+            class = c("euler", "list"))
 }
 
 #' @describeIn euler A matrix of logical vectors with columns representing sets
@@ -239,7 +240,7 @@ euler.default <- function(combinations, input = c("disjoint", "union"), ...) {
 euler.matrix <- function(combinations, by = NULL, ...) {
   if (!is.null(by)) {
     vapply(by,
-           function(x) assertthat::assert_that(is.factor(x) || is.character(x)),
+           function(x) assert_that(is.factor(x) || is.character(x)),
            FUN.VALUE = logical(1))
     if (is.matrix(by) || is.data.frame(by)) {
       if (ncol(by) > 2)
@@ -247,7 +248,7 @@ euler.matrix <- function(combinations, by = NULL, ...) {
     }
   }
 
-  assertthat::assert_that(
+  assert_that(
     any(is.logical(combinations), is.numeric(combinations)),
     max(combinations, na.rm = TRUE) == 1,
     min(combinations, na.rm = TRUE) == 0,
@@ -265,49 +266,19 @@ euler.matrix <- function(combinations, by = NULL, ...) {
 }
 
 #' @describeIn euler A data.frame that can be converted to a matrix of logicals
-#'   (as in the description above) via \code{\link[base]{as.matrix}}.
+#'   (as in the description above) via [base::as.matrix()].
 #' @export
 euler.data.frame <- function(combinations, by = NULL, ...) {
   euler(as.matrix(combinations), by = by, ...)
-}
-
-#' Print euler fits
-#'
-#' Prints a data frame of the original set relationships and the fitted
-#' values as well as diagError and stress statistics.
-#'
-#' @param x Euler diagram specification from \code{\link{euler}}.
-#' @param round Number of decimal places to round to.
-#' @param ... Arguments passed to \code{\link[base]{print.data.frame}}.
-#'
-#' @return Prints the results of the fit.
-#'
-#' @export
-print.euler <- function(x, round = 3, ...) {
-  assertthat::assert_that(
-    assertthat::is.number(round)
-  )
-  out <- data.frame(
-    "original" = x$original.values,
-    "fitted" = x$fitted.values,
-    "residuals" = x$residuals,
-    "region_error" = x$region_error
-  )
-  print(round(out, digits = round), ...)
-  cat("\n")
-  cat("diag_error: ", round(x$diag_error, digits = round), "\n")
-  cat("stress:     ", round(x$stress, digits = round), "\n")
 }
 
 #' @describeIn euler A list of vectors, each vector giving the contents of
 #'   that set. Vectors in the list do not need to be named.
 #' @export
 euler.list <- function(combinations, ...) {
-  assertthat::assert_that(
-    assertthat::has_attr(combinations, "names"),
-    !any(names(combinations) == ""),
-    !any(duplicated(names(combinations)))
-  )
+  assert_that(has_attr(combinations, "names"),
+              !any(names(combinations) == ""),
+              !any(duplicated(names(combinations))))
 
   sets <- names(combinations)
   n <- length(sets)
@@ -317,9 +288,8 @@ euler.list <- function(combinations, ...) {
   out <- integer(nrow(id))
   names(out) <- apply(id, 1, function(x) paste(sets[x], collapse = "&"))
 
-  for (i in 1:nrow(id)) {
+  for (i in 1:nrow(id))
     out[i] <- length(Reduce(intersect, combinations[id[i, ]]))
-  }
 
   euler(out, input = "union", ...)
 }
