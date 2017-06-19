a3f8e9b9e234bb0abffcdb63d196d9950bdc8339
Johan Larsson <johanlarsson@outlook.com>
2017-06-19 21:57:11 +0200

    Import grDevices, stats, lattice, assertthat, grid.

--- R/eulerr.R
+++ R/eulerr.R
@@ -1,3 +1,9 @@
 #' @useDynLib eulerr, .registration = TRUE
 #' @importFrom Rcpp evalCpp
+#' @import stats
+#' @import grDevices
+#' @importFrom assertthat assert_that is.number is.flag has_attr not_empty
+#' @importFrom lattice trellis.par.get xyplot panel.text lattice.options
+#'   panel.superpose packet.number
+#' @importFrom grid gpar grid.circle
 NULL
