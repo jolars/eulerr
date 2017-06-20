ec398b60d22f6a11027c9204edab30b67da8adfe
Johan Larsson <johanlarsson@outlook.com>
2017-06-20 08:49:54 +0200

    Update tests to match new usage.

--- tests/testthat/test_inputs.R
+++ tests/testthat/test_inputs.R
@@ -1,7 +1,6 @@
 context("Test inputs to euler")
 
 test_that("erroneous named numeric vectors returns errors", {
-  expect_error(euler(c(A = 1)))
   expect_error(euler(c(1, 2, 3)))
   expect_error(euler(c()))
   expect_error(euler(c(A = FALSE, B = TRUE, C = FALSE)))
@@ -33,14 +32,14 @@ test_that("erroneous input using by argument return errors", {
 
 test_that("arguments to print.euler are specified correctly", {
   f <- euler(c(A = 10.923, B = 5.4, "A&B" = 0.43))
-  expect_error(print(f, round = "hello"))
-  expect_error(print(f, round = c(1, 2)))
+  expect_error(dont_print(f, round = "hello"))
+  expect_error(dont_print(f, round = c(1, 2)))
 })
 
 test_that("normal use returns no errors", {
   f <- euler(c(A = 10.923, B = 5.4, "A&B" = 0.43))
 
-  expect_error(print(f, round = 2), NA)
+  expect_error(dont_print(f, round = 2), NA)
 
   dat <- data.frame(
     A = sample(c(TRUE, FALSE), size = 100, replace = TRUE),
@@ -51,7 +50,7 @@ test_that("normal use returns no errors", {
   expect_error(euler(dat[, 1:2]), NA)
   expect_error(euler(as.matrix(dat[, 1:2])), NA)
   expect_error(euler(dat[, 1:2], by = dat[, 3]), NA)
-  expect_error(print(euler(dat[, 1:2], by = dat[, 3])), NA)
+  expect_error(dont_print(euler(dat[, 1:2], by = dat[, 3])), NA)
 })
 
 test_that("impossible configurations throw errors", {
--- tests/testthat/test_plotting.R
+++ tests/testthat/test_plotting.R
@@ -2,16 +2,14 @@ context("Test plot functions")
 
 test_that("erroneous input to plot.euler return errors", {
   f <- euler(c(A = 5, B = 2))
-  expect_error(p1 <- plot(f, fill_opacity = "f"))
-  expect_error(p1 <- plot(f, fill_opacity = c(2, 3)))
+  expect_error(p1 <- plot(f, alpha = "f"))
+  expect_error(p1 <- plot(f, alpha = c(2, 3)))
 })
 
 test_that("normal plotting works without errors", {
-  ff <- tempfile()
-  png(filename = ff)
   f1 <- euler(c("A" = 10, "B" = 5, "A&B" = 2))
 
-  expect_error(plot(
+  expect_error(dont_plot(
     f1,
     fill = c("black", "blue"),
     labels = c("Yes", "No"),
@@ -19,9 +17,9 @@ test_that("normal plotting works without errors", {
     fontface = 4
   ), NA)
 
-  expect_error(plot(f1), NA)
-  expect_error(plot(f1, key = TRUE, counts = TRUE), NA)
-  expect_error(plot(f1, labels = c("asdf", "qwer")), NA)
+  expect_error(dont_plot(f1), NA)
+  expect_error(dont_plot(f1, auto.key = TRUE, counts = TRUE), NA)
+  expect_error(dont_plot(f1, labels = c("asdf", "qwer")), NA)
 
   dat <- data.frame(
     Liberal = sample(c(TRUE, FALSE), size = 100, replace = TRUE),
@@ -31,13 +29,11 @@ test_that("normal plotting works without errors", {
   )
 
   f2 <- euler(dat[, 1:2], by = dat[, 3:4])
-  f3 <- euler(dat[, 1:2], by = dat[, 3])
+  f3 <- euler(dat[, 1:2], by = dat[, 3, drop = FALSE])
 
-  expect_error(plot(f2, fill = "transparent", lty = c(1, 2),
-                    lwd = c(1, 2)), NA)
-  expect_error(plot(f3), NA)
-  dev.off()
-  unlink(ff)
+  expect_error(dont_plot(f2, fill = "transparent", lty = c(1, 2),
+                         lwd = c(1, 2)), NA)
+  expect_error(dont_plot(f3), NA)
 })
 
 
