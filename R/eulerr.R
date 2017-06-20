d2c9cb6fd950919e85f5a3da532057abff7ab5f0
Johan Larsson <johanlarsson@outlook.com>
2017-06-20 08:51:04 +0200

    Explicit imports.

--- R/eulerr.R
+++ R/eulerr.R
@@ -1,7 +1,9 @@
 #' @useDynLib eulerr, .registration = TRUE
 #' @importFrom Rcpp evalCpp
-#' @import stats
-#' @import grDevices
+#' @importFrom grDevices adjustcolor dev.off png xy.coords
+#' @importFrom graphics plot
+#' @importFrom stats optim nlm as.formula coef optimize runif
+#' @importFrom utils capture.output modifyList
 #' @importFrom assertthat assert_that is.number is.flag has_attr not_empty
 #' @importFrom lattice trellis.par.get xyplot panel.text lattice.options
 #'   panel.superpose packet.number
