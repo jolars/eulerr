f406370ea3263d84bcbf00c6182de5807f75057e
Johan Larsson <johanlarsson@outlook.com>
2017-06-20 08:51:54 +0200

    drop explicit namespace call.

--- R/optimization.R
+++ R/optimization.R
@@ -37,7 +37,7 @@ initial_layout_gradient <- function(par, distances, disjoint, contained, two) {
 # Optimization function for disc_disc intersection ------------------------
 
 separate_two_discs <- function(r1, r2, overlap) {
-  stats::optimize(
+  optimize(
     function(x, r1, r2, overlap) (discdisc(r1, r2, d = x) - overlap) ^ 2L,
     interval = c(abs(r1 - r2), sum(r1, r2)),
     r1 = r1,
