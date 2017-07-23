24b38935222ec4a3cdca315ceaaf6303ce0021ab
Johan Larsson <johanlarsson@outlook.com>
2017-07-23 20:02:42 +0200

    Move helper functions to separate header.

--- src/helpers.h
+++ src/helpers.h
@@ -1,48 +1,14 @@
-// [[Rcpp::depends(RcppArmadillo)]]
-// [[Rcpp::plugins(cpp11)]]
+#ifndef HELPERS_H_
+#define HELPERS_H_
 
 #include <RcppArmadillo.h>
-using namespace Rcpp;
-using namespace arma;
 
 // Set intersect
-inline arma::uvec set_intersect(const arma::urowvec x, const arma::urowvec y) {
+inline arma::uvec set_intersect(const arma::uvec& x, const arma::uvec& y) {
   std::vector<int> out;
-  std::set_intersection(x.begin(),x.end(), y.begin(), y.end(),
+  std::set_intersection(x.begin(), x.end(), y.begin(), y.end(),
                         std::back_inserter(out));
   return arma::conv_to<arma::uvec>::from(out);
-}
-
-template <typename T>
-arma::Mat<T> adjoint(const arma::Mat<T> M) {
-  arma::Mat<T> out(3, 3);
-  T a = M(0, 0),
-    b = M(1, 0),
-    c = M(1, 1),
-    d = M(2, 0),
-    e = M(2, 1),
-    f = M(2, 2);
-
-  out(0, 0) = c*f - e*e;
-  out(1, 0) = d*e - b*f;
-  out(1, 1) = a*f - d*d;
-  out(2, 0) = b*e - c*d;
-  out(2, 1) = b*d - a*e;
-  out(2, 2) = a*c - b*b;
-
-  return symmatl(out);
-}
-
-template <typename T>
-arma::Mat<T> skewsymmat(const arma::Col<T>& v) {
-  arma::Mat<T> out(3, 3, fill::zeros);
-  out(0, 1) =  v(2);
-  out(0, 2) = -v(1);
-  out(1, 0) = -v(2);
-  out(1, 2) =  v(0);
-  out(2, 0) =  v(1);
-  out(2, 1) = -v(0);
-  return out;
 }
 
 // Number of n choose k. (Credited to Ben Voigt.)
@@ -51,9 +17,9 @@ inline arma::uword nck(arma::uword n, arma::uword k) {
   if (k * 2 > n) k = n - k;
   if (k == 0) return 1;
 
-  uword out = n;
+  arma::uword out = n;
 
-  for(uword i = 2; i <= k; ++i) {
+  for(arma::uword i = 2; i <= k; ++i) {
     out *= (n - i + 1);
     out /= i;
   }
@@ -62,20 +28,19 @@ inline arma::uword nck(arma::uword n, arma::uword k) {
 
 // Bit indexing
 // (http://stackoverflow.com/questions/9430568/generating-combinations-in-c)
-template <class C>
-arma::umat bit_index(C n) {
+inline arma::umat bit_index(arma::uword n) {
   arma::uword n_combos = 0;
 
   for (arma::uword i = 1; i < n + 1; i++)
     n_combos += nck(n, i);
 
-  arma::umat out(n_combos, n, fill::zeros);
+  arma::umat out(n_combos, n, arma::fill::zeros);
 
   for (arma::uword i = 1, k = 0; i < n + 1; i++) {
     std::vector<bool> v(n);
     std::fill(v.begin(), v.begin() + i, true);
     do {
-      for (uword j = 0; j < n; ++j) {
+      for (arma::uword j = 0; j < n; ++j) {
         if (v[j]) out(k, j) = true;
       }
       k++;
@@ -84,5 +49,4 @@ arma::umat bit_index(C n) {
   return out;
 }
 
-
-
+#endif // HELPERS_H_
