4a72b9da6bdbb02e224aef12c35188c52f5d91d2
Johan Larsson <johanlarsson@outlook.com>
2017-07-23 20:00:55 +0200

    Call optimize explicitly from stats.

--- R/geometry.R
+++ R/geometry.R
@@ -23,7 +23,7 @@ dist_point_circle <- function(x, y, h, k, r) {
 #'   area.
 #' @keywords internal
 separate_two_discs <- function(r1, r2, overlap) {
-  optimize(
+  stats::optimize(
     function(x, r1, r2, overlap) (discdisc(r1, r2, d = x) - overlap) ^ 2L,
     interval = c(abs(r1 - r2), sum(r1, r2)),
     r1 = r1,
