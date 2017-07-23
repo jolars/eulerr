3949879dc39c8bc7463a92bf7f731b7a5a240861
Johan Larsson <johanlarsson@outlook.com>
2017-07-23 20:02:17 +0200

    Get rid of using namespace.

--- src/utils.cpp
+++ src/utils.cpp
@@ -1,18 +1,14 @@
 // [[Rcpp::depends(RcppArmadillo)]]
 // [[Rcpp::plugins(cpp11)]]
 
-#include <RcppArmadillo.h>
 #include "helpers.h"
-using namespace Rcpp;
-using namespace arma;
-
 
 // [[Rcpp::export]]
-arma::umat choose_two(const arma::uvec x) {
-  uword n = x.size();
-  umat m(n * (n - 1) / 2, 2);
-  for (uword i = 0, k = 0; i < n - 1; i++) {
-    for (uword j = i + 1; j < n; j++, k++) {
+arma::umat choose_two(const arma::uvec& x) {
+  arma::uword n = x.size();
+  arma::umat m(n * (n - 1) / 2, 2);
+  for (arma::uword i = 0, k = 0; i < n - 1; i++) {
+    for (arma::uword j = i + 1; j < n; j++, k++) {
       m(k, 0) = x(i);
       m(k, 1) = x(j);
     }
@@ -22,18 +18,18 @@ arma::umat choose_two(const arma::uvec x) {
 
 // [[Rcpp::export]]
 Rcpp::LogicalMatrix bit_indexr(const arma::uword n) {
-  return wrap(bit_index(n));
+  return Rcpp::wrap(bit_index(n));
 }
 
 // [[Rcpp::export]]
 Rcpp::NumericVector discdisc(
-    const Rcpp::NumericVector r1,
-    const Rcpp::NumericVector r2,
-    const Rcpp::NumericVector d
+    const Rcpp::NumericVector& r1,
+    const Rcpp::NumericVector& r2,
+    const Rcpp::NumericVector& d
   ) {
-  NumericVector r1e = pow(r1, 2);
-  NumericVector r2e = pow(r2, 2);
-  NumericVector de = pow(d, 2);
+  Rcpp::NumericVector r1e = Rcpp::pow(r1, 2);
+  Rcpp::NumericVector r2e = Rcpp::pow(r2, 2);
+  Rcpp::NumericVector de  = Rcpp::pow(d, 2);
 
   return r1e * acos((de + r1e - r2e) / (2 * d * r1)) +
     r2e * acos((de + r2e - r1e) / (2 * d * r2)) -
@@ -42,30 +38,38 @@ Rcpp::NumericVector discdisc(
 
 // [[Rcpp::export]]
 Rcpp::LogicalMatrix find_surrounding_sets(
-    const arma::vec xs,
-    const arma::vec ys,
-    const arma::vec x,
-    const arma::vec y,
-    const arma::vec r
+    const arma::vec& xs,
+    const arma::vec& ys,
+    const arma::vec& x,
+    const arma::vec& y,
+    const arma::vec& r
   ) {
-  uword n1 = x.n_elem,
-        n2 = xs.n_elem;
-  umat out(n1, n2);
+  arma::uword n1 = x.n_elem;
+  arma::uword n2 = xs.n_elem;
+  arma::umat out(n1, n2);
 
-  for (uword i = 0; i < n1; i++)
-    for (uword j = 0; j < n2; j++)
+  for (arma::uword i = 0; i < n1; i++)
+    for (arma::uword j = 0; j < n2; j++)
       out(i, j) = (pow(xs(j) - x(i), 2) + pow(ys(j) - y(i), 2) <= pow(r(i), 2));
 
-  return wrap(out);
+  return Rcpp::wrap(out);
 }
 
 // [[Rcpp::export]]
-arma::uword max_colmins(const arma::mat x) {
-  uword n = x.n_cols;
-  vec mins(n);
-  for (uword i = 0; i < n; i++)
+arma::uword max_colmins(const arma::mat& x) {
+  arma::uword n = x.n_cols;
+  arma::vec mins(n);
+  for (arma::uword i = 0; i < n; i++)
     mins(i) = x.col(i).min();
   return mins.index_max() + 1;
+}
+
+// [[Rcpp::export]]
+double venneuler_stress(const arma::vec& areas, const arma::vec& fit) {
+  double sst   = arma::accu(arma::square(fit));
+  double slope = arma::accu(areas % fit) / arma::accu(arma::square(areas));
+  double sse   = arma::accu(arma::square(fit - areas * slope));
+  return sse / sst;
 }
 
 
