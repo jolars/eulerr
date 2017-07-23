239464c3a88cdab16fb020ae3fa4daa70bede42a
Johan Larsson <johanlarsson@outlook.com>
2017-07-23 20:00:40 +0200

    Drop namespace imports.

--- R/eulerr.R
+++ R/eulerr.R
@@ -1,13 +1,6 @@
 #' @useDynLib eulerr, .registration = TRUE
-#' @importFrom Rcpp evalCpp
-#' @importFrom grDevices adjustcolor dev.off png xy.coords
-#' @importFrom graphics plot
-#' @importFrom stats optim nlm as.formula coef optimize runif
-#' @importFrom utils capture.output modifyList
-#' @importFrom assertthat assert_that is.number is.flag has_attr not_empty
-#' @importFrom lattice trellis.par.get xyplot panel.text lattice.options
-#'   panel.superpose packet.number trellis.grobname
-#' @importFrom grid gpar grid.circle
+#' @importFrom Rcpp sourceCpp
+NULL
 
 #' @keywords internal
 "_PACKAGE"
