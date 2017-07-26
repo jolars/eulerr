4f0f68b4475c1a38cf43de796e8c79e177e890c3
Johan Larsson <johanlarsson@outlook.com>
2017-07-26 21:20:45 +0200

    Explicityl call std::

--- src/conversions.h
+++ src/conversions.h
@@ -8,8 +8,8 @@ arma::mat standard_to_matrix(const arma::vec& v) {
   arma::mat::fixed<3, 3> out;
   out.zeros();
 
-  out(0, 0) = pow(v(2), -2);
-  out(1, 1) = pow(v(3), -2);
+  out(0, 0) = std::pow(v(2), -2);
+  out(1, 1) = std::pow(v(3), -2);
   out(2, 2) = -1;
 
   double theta = v(4);
