fac9f8119b2a226d98cfb9f657c50695fc1e5218
Johan Larsson <johanlarsson@outlook.com>
2017-07-26 21:21:42 +0200

    Call std:: and use custom sign function.

--- src/solver.h
+++ src/solver.h
@@ -3,6 +3,7 @@
 
 #include <RcppArmadillo.h>
 #include "constants.h"
+#include "helpers.h"
 
 // Solve a cubic polynomial
 arma::cx_vec solve_cubic(const arma::vec& v) {
@@ -12,16 +13,17 @@ arma::cx_vec solve_cubic(const arma::vec& v) {
   double a = v(1)/v(0);
   double b = v(2)/v(0);
   double c = v(3)/v(0);
-  double Q = (pow(a, 2) - 3.0*b)/9.0;
-  double R = (2.0*pow(a, 3) - 9.0*a*b + 27.0*c)/54.0;
+  double Q = (std::pow(a, 2) - 3.0*b)/9.0;
+  double R = (2.0*std::pow(a, 3) - 9.0*a*b + 27.0*c)/54.0;
 
-  if (pow(R, 2) < pow(Q, 3)) {
-    double theta = acos(R/sqrt(pow(Q, 3)));
-    y(0) = -2.0*sqrt(Q)*cos(theta/3.0) -  a/3.0;
-    y(1) = -2.0*sqrt(Q)*cos((theta + 2.0*arma::datum::pi)/3.0) - a/3.0;
-    y(2) = -2.0*sqrt(Q)*cos((theta - 2.0*arma::datum::pi)/3.0) - a/3.0;
+  if (std::pow(R, 2) < std::pow(Q, 3)) {
+    double theta = std::acos(R/std::sqrt(std::pow(Q, 3)));
+    y(0) = -2.0*std::sqrt(Q)*std::cos(theta/3.0) -  a/3.0;
+    y(1) = -2.0*std::sqrt(Q)*std::cos((theta + 2.0*arma::datum::pi)/3.0) - a/3.0;
+    y(2) = -2.0*std::sqrt(Q)*std::cos((theta - 2.0*arma::datum::pi)/3.0) - a/3.0;
   } else {
-    double A = -copysign(1.0, R)*cbrt(std::abs(R) + sqrt(pow(R, 2) - pow(Q, 3)));
+    double A =
+      -sign(R)*std::cbrt(std::abs(R) + std::sqrt(pow(R, 2) - std::pow(Q, 3)));
     double B;
     if (std::abs(A - 0) < small) {
       B = 0;
@@ -29,8 +31,8 @@ arma::cx_vec solve_cubic(const arma::vec& v) {
       B = Q/A;
     }
     y(0) = A + B - a/3.0;
-    y(1) = -0.5*(A + B) - a/3.0 + sqrt(3.0)*i*(A - B)/2.0;
-    y(2) = -0.5*(A + B) - a/3.0 - sqrt(3.0)*i*(A - B)/2.0;
+    y(1) = -0.5*(A + B) - a/3.0 + std::sqrt(3.0)*i*(A - B)/2.0;
+    y(2) = -0.5*(A + B) - a/3.0 - std::sqrt(3.0)*i*(A - B)/2.0;
   }
 
   return y;
