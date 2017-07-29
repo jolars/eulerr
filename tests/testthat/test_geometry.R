59a870dbd4e9253ec1fbbad7af7f8a4881939b4b
Johan Larsson <johanlarsson@outlook.com>
2017-07-29 17:23:33 +0200

    Drop unused geometry tests.

--- tests/testthat/test_geometry.R
+++
@@ -1,8 +0,0 @@
-context("Test geometry functions")
-
-test_that("find_sets_containing_points function works", {
-  res <- find_surrounding_sets(1:10, 1:10, 2, 2, 4)
-  expect_equal(sum(res), 4)
-
-  expect_equal(dist_point_circle(-4, -11, 4, -5, 7), 3)
-})
