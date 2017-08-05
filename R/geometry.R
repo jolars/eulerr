3022ab07be05f94224aaf64a87af0daba11b0111
Johan Larsson <johanlarsson@outlook.com>
2017-08-05 15:48:43 +0200

    Avoid the wrapper to discdisc.

--- R/geometry.R
+++ R/geometry.R
@@ -8,14 +8,10 @@
 #'   area.
 #' @keywords internal
 separate_two_discs <- function(r1, r2, overlap) {
-  # TODO: Move the entire discdisc function to c++
-  stats::optimize(
-    function(x, r1, r2, overlap) (discdisc(r1, r2, d = x) - overlap) ^ 2L,
-    interval = c(abs(r1 - r2), sum(r1, r2)),
-    r1 = r1,
-    r2 = r2,
-    overlap = overlap,
-    tol = sqrt(.Machine$double.eps)
-  )$minimum
+  stats::optimize(discdisc,
+                  interval = c(abs(r1 - r2), sum(r1, r2)),
+                  r1 = r1,
+                  r2 = r2,
+                  overlap = overlap,
+                  tol = sqrt(.Machine$double.eps))$minimum
 }
-
