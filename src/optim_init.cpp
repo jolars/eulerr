86a98059fadc213dcc9942f42ef444b417706820
Johan Larsson <johanlarsson@outlook.com>
2017-08-06 12:10:43 +0200

    Unify gradient and loss function, which is what nlm() wants anyway.

--- src/optim_init.cpp
+++ src/optim_init.cpp
@@ -5,55 +5,35 @@
 
 // #define ARMA_NO_DEBUG
 
-// Loss function for the intial optimizer.
+// Loss and gradient for the initial optimizer.
 // [[Rcpp::export]]
-double optim_init_loss(const arma::vec& par,
-                       const arma::vec& d,
-                       const arma::uvec& disjoint,
-                       const arma::uvec& contained) {
+Rcpp::NumericVector optim_init(const arma::rowvec& par,
+                               const arma::vec& d,
+                               const arma::uvec& disjoint,
+                               const arma::uvec& contained) {
   arma::uword n = par.n_elem/2;
   arma::mat xy = arma::reshape(par, n, 2).t();
 
-  double out = 0;
+  Rcpp::NumericVector loss(1, 0.0);
+  arma::mat gradMat(2, n, arma::fill::zeros);
   for (arma::uword i = 0, k = 0; i < n; i++) {
     for (arma::uword j = i + 1; j < n; j++, k++) {
       arma::vec xycold = xy.col(i) - xy.col(j);
-      double D = arma::as_scalar(xycold.t() * xycold) - std::pow(d(k), 2);
+      double D = arma::as_scalar(xycold.t()*xycold) - std::pow(d(k), 2);
       if (disjoint(k) && (D >= 0)) {
         continue;
       } else if (contained(k) && (D < 0)) {
         continue;
       } else {
-        out += std::pow(D, 2);
+        loss[0] += std::pow(D, 2);
+        gradMat.col(i) += (4*(D))*xycold;
+        gradMat.col(j) -= (4*(D))*xycold;
       }
     }
   }
-  return out;
-}
 
-// Gradient for the initial optimizer.
-// [[Rcpp::export]]
-SEXP optim_init_grad(const arma::rowvec& par,
-                     const arma::vec& d,
-                     const arma::uvec& disjoint,
-                     const arma::uvec& contained) {
-  arma::uword n = par.n_elem/2;
-  arma::mat xy = arma::reshape(par, n, 2).t();
+  loss.names() = Rcpp::CharacterVector::create("grad");
+  loss.attr("gradient") = Rcpp::wrap(arma::vectorise(gradMat, 1));
 
-  arma::mat out(2, n, arma::fill::zeros);
-  for (arma::uword i = 0, k = 0; i < n; i++) {
-    for (arma::uword j = i + 1; j < n; j++, k++) {
-      arma::vec xycold = xy.col(i) - xy.col(j);
-      double D = arma::as_scalar(xycold.t() * xycold) - std::pow(d(k), 2);
-      if (disjoint(k) && (D >= 0)) {
-        continue;
-      } else if (contained(k) && (D < 0)) {
-        continue;
-      } else {
-        out.col(i) += (4 * (D)) * xycold;
-        out.col(j) -= (4 * (D)) * xycold;
-      }
-    }
-  }
-  return Rcpp::wrap(arma::vectorise(out, 1));
+  return loss;
 }
