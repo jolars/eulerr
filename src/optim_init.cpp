0163314b311ae28d59460e76582aa97d1389fa43
Johan Larsson <johanlarsson@outlook.com>
2017-08-07 13:15:49 +0200

    Upload a as of yet not working hessian computation.

--- src/optim_init.cpp
+++ src/optim_init.cpp
@@ -18,16 +18,16 @@ Rcpp::NumericVector optim_init(const arma::rowvec& par,
   arma::mat gradMat(2, n, arma::fill::zeros);
   for (arma::uword i = 0, k = 0; i < n; i++) {
     for (arma::uword j = i + 1; j < n; j++, k++) {
-      arma::vec xycold = xy.col(i) - xy.col(j);
-      double D = arma::as_scalar(xycold.t()*xycold) - std::pow(d(k), 2);
+      arma::vec xyd = xy.col(i) - xy.col(j);
+      double D = arma::as_scalar(xyd.t()*xyd) - std::pow(d(k), 2);
       if (disjoint(k) && (D >= 0)) {
         continue;
       } else if (contained(k) && (D < 0)) {
         continue;
       } else {
         loss[0] += std::pow(D, 2);
-        gradMat.col(i) += (4*(D))*xycold;
-        gradMat.col(j) -= (4*(D))*xycold;
+        gradMat.col(i) += 4*D*xyd;
+        gradMat.col(j) -= 4*D*xyd;
       }
     }
   }
@@ -36,3 +36,68 @@ Rcpp::NumericVector optim_init(const arma::rowvec& par,
 
   return loss;
 }
+
+
+// // Computes the loss, gradient, and hessian
+// // Currently not working.
+// // [[Rcpp::export]]
+// Rcpp::NumericVector optim_init(const arma::vec& par,
+//                                const arma::mat& d,
+//                                const arma::umat& disjoint,
+//                                const arma::umat& contained) {
+//   arma::uword n = par.n_elem/2;
+//   arma::vec x = par.head(n);
+//   arma::vec y = par.tail(n);
+//
+//   Rcpp::NumericVector loss(1, 0.0);
+//   Rcpp::NumericVector grad(2*n, 0.0);
+//   arma::mat hess(2*n, 2*n, arma::fill::zeros);
+//
+//   for (arma::uword i = 0; i < n; ++i) {
+//     for (arma::uword j = i + 1; j < n; ++j) {
+//       double xd = x(i) - x(j);
+//       double yd = y(i) - y(j);
+//       double D = std::pow(xd, 2) + std::pow(yd, 2) - std::pow(d(i, j), 2);
+//       if (disjoint(i, j) && (D >= 0)) {
+//         continue;
+//       } else if (contained(i, j) && (D < 0)) {
+//         continue;
+//       } else {
+//         loss[0]     += std::pow(D, 2);
+//         grad[i]     += 4*xd*D;
+//         grad[j]     -= 4*xd*D;
+//         grad[i + n] += 4*yd*D;
+//         grad[j + n] -= 4*yd*D;
+//       }
+//     }
+//   }
+//
+//   for (arma::uword i = 0; i < n; ++i) {
+//     for (arma::uword j = i; j < n; ++j) {
+//       double xd = x(i) - x(j);
+//       double yd = y(i) - y(j);
+//       double D = std::pow(xd, 2) + std::pow(yd, 2) - std::pow(d(i, j), 2);
+//       if (disjoint(i, j) && (D >= 0)) {
+//         continue;
+//       } else if (contained(i, j) && (D < 0)) {
+//         continue;
+//       } else {
+//         hess(i, j)         = -4*D - 8*std::pow(xd, 2);
+//         hess(i + n, j)     = -8*xd*yd;
+//         hess(i, j + n)     = -8*xd*yd;
+//         hess(i + n, j + n) = -4*D - 8*std::pow(yd, 2);
+//       }
+//     }
+//   }
+//
+//   hess.submat(0, 0, arma::size(n, n)) = arma::symmatu(hess.submat(0, 0, arma::size(n, n)));
+//   hess.submat(0, n, arma::size(n, n)) = arma::symmatu(hess.submat(0, n, arma::size(n, n)));
+//   hess.submat(n, 0, arma::size(n, n)) = arma::symmatu(hess.submat(n, 0, arma::size(n, n)));
+//   hess.submat(n, n, arma::size(n, n)) = arma::symmatu(hess.submat(n, n, arma::size(n, n)));
+//
+//   loss.attr("gradient") = grad;
+//   loss.attr("hessian") = hess;
+//
+//   return loss;
+// }
+// //4((x−a)2+(y−b)2−d2) + 8(x−a)2
