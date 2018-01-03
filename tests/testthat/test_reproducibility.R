46094617df16b65821b23c48cd6b6d4aae044b75
Johan Larsson <johanlarsson@outlook.com>
2018-01-03 19:03:41 +0100

    Try to speed up test to make travis-ci finish on time for valgrind build.

--- tests/testthat/test_reproducibility.R
+++ tests/testthat/test_reproducibility.R
@@ -96,5 +96,8 @@ test_that("expect no errors for a variety of predefined sets", {
   expect_error(euler(s12, shape = "ellipse"), NA)
   expect_error(euler(s13, shape = "ellipse"), NA)
   expect_error(euler(s14, shape = "ellipse"), NA)
-  expect_error(euler(s15, shape = "ellipse"), NA)
+  expect_error(euler(s15,
+                     shape = "ellipse",
+                     control = list(extraopt_control = list(itermax = 50))),
+               NA)
 })
