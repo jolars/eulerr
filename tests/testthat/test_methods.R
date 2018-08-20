161db38fff904b6a82f58478fc81eda064ad1838
Johan Larsson <johanlarsson@outlook.com>
2018-08-20 17:51:23 +0200

    Add some trivial method tests

---
+++ tests/testthat/test_methods.R
@@ -0,0 +1,7 @@
+context("Methods")
+
+test_that("fitted and coef methods work as they should", {
+  fit <- euler(c(A = 1, B = 2))
+  expect_is(fitted(fit), "numeric")
+  expect_is(coef(fit), "data.frame")
+})
