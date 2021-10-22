45f5e136e31dac4b6e9ee47d02aaf58660e315be
Johan Larsson <johanlarsson@outlook.com>
2021-10-22 22:53:43 +0200

    Don't use using declarations

--- src/utils.cpp
+++ src/utils.cpp
@@ -8,12 +8,11 @@
 arma::umat
 choose_two(const arma::uvec& x)
 {
-  using namespace arma;
+  arma::uword n = x.n_elem;
+  arma::umat m(n * (n - 1) / 2, 2);
 
-  uword n = x.n_elem;
-  umat m(n * (n - 1) / 2, 2);
-  for (uword i = 0, k = 0; i < n - 1; ++i) {
-    for (uword j = i + 1; j < n; ++j, ++k) {
+  for (arma::uword i = 0, k = 0; i < n - 1; ++i) {
+    for (arma::uword j = i + 1; j < n; ++j, ++k) {
       m(k, 0) = x(i);
       m(k, 1) = x(j);
     }
@@ -26,16 +25,14 @@ choose_two(const arma::uvec& x)
 double
 discdisc(double d, double r1, double r2, double overlap)
 {
-  using namespace std;
-
   double r1sq = r1 * r1;
   double r2sq = r2 * r2;
   double dsq  = d * d;
 
-  double D =
-    r1sq * acos((dsq + r1sq - r2sq) / (2 * d * r1)) +
-    r2sq * acos((dsq + r2sq - r1sq) / (2 * d * r2)) -
-    0.5 * sqrt((r1 + r2 - d) * (d + r1 - r2) * (d - r1 + r2) * (d + r1 + r2));
+  double D = r1sq * std::acos((dsq + r1sq - r2sq) / (2 * d * r1)) +
+             r2sq * std::acos((dsq + r2sq - r1sq) / (2 * d * r2)) -
+             0.5 * std::sqrt((r1 + r2 - d) * (d + r1 - r2) * (d - r1 + r2) *
+                             (d + r1 + r2));
 
   return (D - overlap) * (D - overlap);
 }
