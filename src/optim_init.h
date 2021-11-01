edb5f17da3ae10c72ae1bfbc9b9d1dc447a8acf6
Johan Larsson <johanlarsson@outlook.com>
2021-11-01 21:01:45 +0100

    Remove unnecessary headers

--- src/optim_init.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <RcppArmadillo.h>
-
-Rcpp::NumericVector
-optim_init(const Rcpp::NumericVector& par,
-           const Rcpp::NumericMatrix& d,
-           const Rcpp::LogicalMatrix& disjoint,
-           const Rcpp::LogicalMatrix& subset);
